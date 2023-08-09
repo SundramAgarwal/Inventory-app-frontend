fd5e4d33477361a3d105565f6982ed74b5a22dbf
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-09 20:33:01 +0530

    final commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,14 @@
 import React from 'react';
-// import { GiHumanPyramid } from "react-icons/gi"
 import { RiProductHuntLine } from "react-icons/ri";
 import {Link} from 'react-router-dom'
 import './Home.css';
-// import heroImg from '../../assets/img_1485.jpg';
-import heroImg from "../../assets/img_1485.jpg";
+// import heroImg from "../../assets/img_1485.jpg";
 import { ShowOnLogin, ShowOnLogout } from '../../components/protect/HiddenLink';
 const Home = () => {
   return (
     <div className='home --hom'>
         <nav className='container --flex-between'>
             <div className='logo'>
-                {/* <GiHumanPyramid size = {80}/> */}
                 <RiProductHuntLine size={35} />
             </div>
             <ul className='home-links'>
@@ -39,16 +36,13 @@ const Home = () => {
         <section className='container hero'>
 
             <div className='hero-text'>
-                {/* <h2>HUMAN RESOURCE MANAGEMENT</h2> */}
                 <h2>Inventory {"&"} Stock Management Solution</h2>
-                {/* <p>Human Resource Management is to manage the data of University Staff in real time and integrate to make it easier to manage all staff activities. </p> */}
                 <p>
                   Inventory system to control and manage proucts in the warehouse in
                   real timeand integrated to make it easier to develop your business.
                 </p>
                 <div className='hero-buttons'>
                     <button className='--btn --btn-secondary'>
-                        {/* <Link to='/minorDetails'>Minor Details of Employees.</Link> */}
                         <Link to="/dashboard">Free Trial 1 Month</Link>
                     </button>
                 </div>
@@ -60,7 +54,6 @@ const Home = () => {
             </div> 
 
             <div className='hero-image' >
-                {/* <img src={heroImg}  alt = "IKGPTU HR Management"></img> */}
                 <img src='https://www.akgvg.com/blog/wp-content/uploads/2021/06/Inventory-Management.jpg' alt="Inventory" style={{ width: '600px', height: '400px' }}  />
             </div>
         </section>
